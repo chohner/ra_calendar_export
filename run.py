eafe5742ba1d9d2217b168f90304c36ad253626a
chohner <mail@chohner.com>
2020-03-09 10:18:46 +0100

    reorder functions

--- run.py
+++ run.py
@@ -6,6 +6,12 @@ from click.testing import CliRunner
 import black
 import pytest
 
+def ics_export():
+    from ra_calendar_export import main  # importing at top of file would screw up coverage
+    username = input("username:")
+    password = getpass("password (only for private profiles):")
+    main.ics_file_from_profile(username, password)
+
 def lint():
     code_root = Path(__file__).parent
     runner = CliRunner()
@@ -21,10 +27,3 @@ def lint():
 def test():
     test_result = pytest.main(['--cov=ra_calendar_export', '--cov-report', 'term-missing'])
     sys.exit(0) if test_result == pytest.ExitCode.OK else sys.exit(1)
-
-def ics_export():
-    from ra_calendar_export import main  # importing at top of file would screw up coverage
-    username = input("username:")
-    password = getpass("password (only for private profiles):")
-    main.ics_file_from_profile(username, password)
-
